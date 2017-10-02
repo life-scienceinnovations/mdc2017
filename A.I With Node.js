ddf011e8e2d4415e7af3794e5c6514ab920c609b
Bryson <bryson.rafael@gmail.com>
2017-10-02 10:44:01 -0500

    A.I. With Node.js

---
+++ A.I With Node.js
@@ -0,0 +1,26 @@
+What is A.I. ?
+
+	"The art of creating machines that perform functions that require intelligence when performed by people."
+
+A.I.Seeing App - Seeing Eye
+
+Intelligence APIs
+
+Vision - Computer Vision API, Emotion API, Face API, Video API
+
+Speech - Customer recognition intelligence service(CRIS), Speaker recognition API, Speech to Text API, Text to Speech API
+
+Language - Language understanding intelligence service(LUIS), Linguistic analytics API, Bing spell check API, Text analytics API, Web language model API
+
+Knowledge - Academic knowledge API, Entity linking intelligence service, Knowledge exploration service, Recommendations APIs
+
+Search - Web search API, Bing image search API, Bing news search API, Bing video search API, auto suggest API
+
+	* These are all microsoft api 's available on their site, accessible via RESTful services
+
+node modules: {
+	"project-oxford": https: //azure.microsoft.com/en-us/services/cognitive-services/ - facial recognition,
+		"LUIS": https: //www.luis.ai/home - conversational intelligence,
+}
+
+* Include the module instead of using RESTful API
